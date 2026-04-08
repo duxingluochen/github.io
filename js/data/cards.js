6edcdcfcb271a271eedb0f5ccba9ddb47720bb6b
yuan <2577381598@qq.com>
2026-04-08 13:27:33 +0800

    增加友链

--- js/data/cards.js
+++ js/data/cards.js
@@ -131,4 +131,6 @@ addCard("video_parse", "VIP视频解析", "全网视频解析接口", "https://h
 addCard("video_parse", "解析工具聚合", "多线路解析", "https://v.zhenlang.cc/", "fas fa-link");
 addCard("friend_link", "小众软件论坛", "软硬玩家聚集地", "https://meta.appinn.net/", "fas fa-users");
 addCard("friend_link", "知乎", "知识社区", "https://www.zhihu.com/", "fab fa-zhihu");
-addCard("friend_link","小光资源库","涵盖各类型,总资源10000T+","https://linkdd.cn/guangyu","img/DDlink-20241015173156.jpg")
+addCard("friend_link","小光资源库","涵盖各类型,总资源10000T+","https://linkdd.cn/guangyu","img/DDlink-20241015173156.jpg");
+addCard("friend_link","查123","实用导航","https://www.cha123.xyz","fas fa-lightbulb");
+addCard("friend_link","搬砖的码农","分享技术，闲聊扯淡","https://refblogs.com/","fas fa-lightbulb")
